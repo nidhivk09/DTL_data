fb86b4e8c918829928ef085b349a36fea0d82d2a
nidhivk09 <nidhivk09@gmail.com>
2024-10-08 21:06:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import altair as alt
+from annotated_text import annotated_text
 
 
 st.set_page_config(
@@ -15,6 +16,6 @@ alt.themes.enable("dark")
 df=pd.read_csv('responses.csv')
 
 st.title('AR/VR powered Immersive Study Space 📚')
-st.image('books.png',use_column_width=True)
+#st.image('books.png',use_column_width=True)
 
-st.subheader('1. Meet or stakeholders.')
+annotated_text(st.subheader('1. Meet or stakeholders.'))
