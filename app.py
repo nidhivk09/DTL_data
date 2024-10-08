46cc8ba6d402db75a4c55a5dde6fde0e34abbe08
nidhivk09 <nidhivk09@gmail.com>
2024-10-08 22:29:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ st.write("Analysis: Most people spend around 1-2hrs studying, which gives us ins
 st.subheader('4. Do they have a dedicated study space?')
 
 st.image('dedicatedstudyspace.png')
-st.write("Almost 3/4th of our responders have a dedicated study space")
+st.write("Analysis: Almost 3/4th of our responders have a dedicated study space")
 
 
 
@@ -52,7 +52,7 @@ st.write("Almost 3/4th of our responders have a dedicated study space")
 st.subheader('5. How is their current study space?')
 
 st.image('currentstudy.png')
-st.write("Our responders studied in their room at home or hostel room with only a minority of them studying in a library of with friends")
+st.write("Analysis: Our responders studied in their room at home or hostel room with only a minority of them studying in a library of with friends")
 
 
 
@@ -61,19 +61,21 @@ st.write("Our responders studied in their room at home or hostel room with only 
 st.subheader('6. Does the current space motivate them to be productive?')
 
 st.image('iscureentspacegood.png')
-st.write("Most people were unsure of whether their study space motivated them to be productive")
+st.write("Analysis: Most people were unsure of whether their study space motivated them to be productive")
 
 
 
 st.subheader('7.How familiar are our stakeholders about the AR/ VR technology?')
 st.image('doyouknowarvr.png')
-st.write("Almost 60-70% had good to excellent knowledge about AR/VR technology, which gives useful insights about our potential user base")
+st.write("Analysis: Almost 60-70% had good to excellent knowledge about AR/VR technology, which gives useful insights about our potential user base")
 
 
 
 st.subheader('**8. Is it important to customize study space?**')
 
 st.image('studyspacepersonal.png')
+st.write("Analysis: Around 70% of the people who responded really felt the need to customize their study space, which shows a strong support to our problem statement")
+
 
 
 
@@ -81,6 +83,8 @@ st.image('studyspacepersonal.png')
 st.header('9. Are stakeholders open to integrate AR/VR to set up their study spaces?')
 
 st.image('opentousearinstudt.png')
+st.write("Analysis: Majority of the responders were in favour to integrate AR/VR to set up their study spaces")
+
 
 
 
@@ -88,13 +92,17 @@ st.image('opentousearinstudt.png')
 st.subheader('10. What are the main features to be included in the product?')
 
 st.image('features.png')
+st.write("Analysis: People really supported the progress tracking feature followed by the ability to customize their virtual spaces along with music preferences and assorted study realms")
 
 
 
 
-st.subheader('11. Are the user willing to be a part in the testing pahse of the product development?')
+st.subheader('11. Are the user willing to be a part in the testing phase of the product development?')
+
 
 st.image('betatester.png')
+
+st.write("Analysis: Many responders wanted more details before they would test our product")
 
 
 
