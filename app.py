23e586059a7f4af3d9c3446bbc21459907f81b9c
nidhivk09 <nidhivk09@gmail.com>
2024-10-08 19:38:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import altair as alt
 
 
-
+st.set_page_config(
+  page_title="AR/VR powered Immersive Study Space",
+  page_icon="📚",
+  layout="wide",
+  initial_sidebar_state="expanded")
+  
